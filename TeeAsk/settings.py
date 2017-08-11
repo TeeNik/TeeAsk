4cfe02251ebbbb1d0dd61560553cadca9af4750e
TeeNik <teenik3315@gmail.com>
2017-08-12 01:25:37 +0300

    settings

--- TeeAsk/settings.py
+++ TeeAsk/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'TeeAsk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
+STATICFILES_DIRS =(os.path.join(BASE_DIR,'static',),)
--- TeeAsk/views.py
+++ TeeAsk/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 
+def index(request):
+    title = 'TeeAsk'
+    return render_to_response('index.html', locals())
